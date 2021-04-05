2e7eaf04ed8eeabea7db6e992170d58a3dfc7ecb
vubao2303 <bbbvut@gmail.com>
2021-04-05 13:22:48 -0700

    provide first path /api for routes in the outside index.js

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+// API Routes 
+// cái này sẽ thành /api/ bấy cứ cái gì o trong cái folder api 
+// cứ thích bày ra một bước vậy 
+// nhưng nếu có 1 cái route khac cần giống đít nhưng khác đầu thì có thể vô đây change it 
+router.use("/api", apiRoutes);
+
+// if no API routes are hit, send the react app 
+router.use(function (req, res) {
+  res.sendFile(path.join(_dirname, "../client/build/index.html"));
+});
+
+module.exports = router
